1d18fa208f574f0876050adc5eb187a92b087e9f
spring2018 <spring2018@SELAB8-003>
2025-04-16 15:30:24 +0500

    this is new branch commit

---
+++ ConsoleApp3/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class student
+    {
+        public int id;
+        public string name;
+        public int age;
+    }
+}
